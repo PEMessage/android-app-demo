f1360db613298c057a0e7d3ef2c8f28b25665396
PEMessage <1165739182@qq.com>
2025-07-12 00:16:45 +0800

    add minimal source and gradle script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,31 +6,24 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    id("com.android.application")
 }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
-
-dependencies {
-    // Use JUnit test framework.
-    testImplementation(libs.junit)
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
-}
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+android {
+    compileSdk = 33
+    namespace = "com.example.myapplication"
+    buildFeatures {
+        viewBinding = true
     }
-}
-
-application {
-    // Define the main class for the application.
-    mainClass = "org.example.App"
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 15
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath(kotlin("gradle-plugin", version = "1.9.0"))
+    }
+}
