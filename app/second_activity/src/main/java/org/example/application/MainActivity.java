40d689011ee5c20bd450ae9d8172ecfdfc2d2d33
PEMessage <1165739182@qq.com>
2025-07-21 00:37:00 +0800

    second_activity: init
    Detail:
        ( gcd ; ./gradlew :second_activity:installDebug ; adb shell am start 'org.example.application/.MainActivity')

---
+++ app/second_activity/src/main/java/org/example/application/MainActivity.java
@@ -0,0 +1,20 @@
+package org.example.application;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+}
