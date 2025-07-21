43c40300096b67cf73d5760f89bf26e3aa9ca8c8
PEMessage <1165739182@qq.com>
2025-07-22 00:27:31 +0800

    system_properties: do loading system prop

--- app/system_properties/src/main/java/org/example/application/MainActivity.java
+++ app/system_properties/src/main/java/org/example/application/MainActivity.java
@@ -6,14 +6,31 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.view.Window;
 
+import java.util.Properties;
+import java.util.Enumeration;
+
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Get all system properties
+        Properties systemProperties = System.getProperties();
+        StringBuilder propertiesText = new StringBuilder();
+
+        // Iterate through all properties and build a string
+        Enumeration<?> propertyNames = systemProperties.propertyNames();
+        while (propertyNames.hasMoreElements()) {
+            String key = (String) propertyNames.nextElement();
+            String value = systemProperties.getProperty(key);
+            propertiesText.append(key).append(" = ").append(value).append("\n\n");
+        }
+
         TextView textView = new TextView(this);
-        textView.setText("Hello world");
+        textView.setText(propertiesText.toString());
+        
         setContentView(textView);
     }
 }
