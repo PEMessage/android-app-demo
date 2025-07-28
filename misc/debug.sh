f4adf0d7c8b9245140dde92946c474391198a278
PEMessage <1165739182@qq.com>
2025-07-29 00:35:45 +0800

    also stop pervious instance

--- misc/debug.sh
+++ misc/debug.sh
@@ -4,6 +4,7 @@
 # 设置 > 开发者选项 > 选择调试应用，并从列表中选择您的应用，然后点击等待调试程序
 
 # Start the application's MainActivity
+adb shell force-stop org.example.application
 adb shell am start 'org.example.application/.MainActivity'
 sleep 1
 uid=$(adb shell ps -A | grep org.example.application | awk '{print $2}')
