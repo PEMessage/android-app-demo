7e009cd76c5af8df70619ccf0a49439cc24ecb76
PEMessage <1165739182@qq.com>
2025-08-19 00:06:06 +0800

    heap_info: first work demo, using android.os.Debug.MemoryInfo

--- app/heap_info/src/main/java/org/example/application/MainActivity.java
+++ app/heap_info/src/main/java/org/example/application/MainActivity.java
@@ -1,19 +1,49 @@
 package org.example.application;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Debug;
 import android.widget.TextView;
-import android.view.Window;
-
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         TextView textView = new TextView(this);
-        textView.setText("Hello world");
+        textView.setText(getHeapInfo());
         setContentView(textView);
+    }
+
+    private String getHeapInfo() {
+        // Java heap info
+        Runtime runtime = Runtime.getRuntime();
+        long javaMax = runtime.maxMemory();
+        long javaTotal = runtime.totalMemory();
+        long javaFree = runtime.freeMemory();
+        long javaUsed = javaTotal - javaFree;
+
+        // Native heap info
+        Debug.MemoryInfo memoryInfo = new Debug.MemoryInfo();
+        Debug.getMemoryInfo(memoryInfo);
+        long nativeHeapSize = memoryInfo.getTotalPss() * 1024L;
+        long nativeHeapAllocated = memoryInfo.dalvikPss * 1024L;
+
+        return "Java Heap:\n" +
+               "Max: " + formatSize(javaMax) + "\n" +
+               "Total: " + formatSize(javaTotal) + "\n" +
+               "Used: " + formatSize(javaUsed) + "\n" +
+               "Free: " + formatSize(javaFree) + "\n\n" +
+               "Native Heap:\n" +
+               "Size: " + formatSize(nativeHeapSize) + "\n" +
+               "Allocated: " + formatSize(nativeHeapAllocated);
+    }
+
+    private String formatSize(long bytes) {
+        if (bytes < 1024) return bytes + " B";
+        int exp = (int)(Math.log(bytes) / Math.log(1024));
+        String pre = "KMGTPE".charAt(exp-1) + "B";
+        return String.format("%.1f %s", bytes / Math.pow(1024, exp), pre);
     }
 }
