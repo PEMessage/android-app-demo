3db26fe742fdab7b4e8337c1827dc39e6d701808
PEMessage <1165739182@qq.com>
2025-09-12 00:49:06 +0800

    classscanner_fragment: add state count

--- app/classscanner_fragment/src/main/java/org/example/application/framework/ItemViewHolder.java
+++ app/classscanner_fragment/src/main/java/org/example/application/framework/ItemViewHolder.java
@@ -10,7 +10,11 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.stream.StreamSupport;
+
 import org.example.application.R;
+import org.example.application.framework.MethodItem.State;
 
 public class ItemViewHolder extends AbstractItemViewHolder {
     LinearLayout container;
@@ -28,18 +32,19 @@ public class ItemViewHolder extends AbstractItemViewHolder {
         // Get references to the views
         this.container = (LinearLayout) view;
         this.name = view.findViewById(R.id.iv_name);
-        this.success_count = view.findViewById(R.id.iv_total);
+        this.success_count = view.findViewById(R.id.iv_success_count);
         this.total = view.findViewById(R.id.iv_total);
         this.selected = view.findViewById(R.id.iv_selected);
 
         this.item = item;
 
         selected.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            this.item.allLeaf().forEach(i -> {
-                MethodItem casted = (MethodItem)i;
-                casted.selected = isChecked;
+            FluentIterable.from(this.item.allLeaf())
+                .transform(MethodItem.class::cast)
+                .forEach(i -> {
+                    i.selected = isChecked;
+                });
             });
-        });
         
     }
 
@@ -51,6 +56,14 @@ public class ItemViewHolder extends AbstractItemViewHolder {
     @Override
     public void bind() {
         name.setText(item.name);
+        success_count.setText(
+            String.valueOf(
+                StreamSupport.stream(item.allLeaf().spliterator(), false)
+                    .map(MethodItem.class::cast)
+                    .mapToInt(i -> i.state == State.SUCCESS ? 1 : 0)
+                    .sum()
+            )
+        );
         total.setText("/" + (item.endIndex - item.startIndex));
         selected.setChecked(
             FluentIterable.from(item.allLeaf())
